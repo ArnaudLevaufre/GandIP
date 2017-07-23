d79412a6ae5199abb0cd9995e31897abe63adbf5
Arnaud Levaufre <arnaud@bananium.fr>
2017-07-23 12:30:50 +0200

    Update license dat

--- gandip.py
+++ gandip.py
@@ -1,6 +1,6 @@
 """
 
-Copyright (c) 2015 Arnaud Levaufre <arnaud@levaufre.name>
+Copyright (c) 2017 Arnaud Levaufre <arnaud@levaufre.name>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
